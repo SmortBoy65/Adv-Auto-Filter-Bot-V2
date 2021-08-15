a3b5e0a27f004ae37ff0c3ee23ac659a703225ee
pdisk <83772019+pdisk@users.noreply.github.com>
2021-08-15 10:07:16 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"⭐{file_size}{file_name}"
+            button_text = f"⭐{file_size} 📂{file_name}"
             
 
             if file_type == "video":
@@ -147,13 +147,13 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("Next ⏭️", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("NEXT ➡️", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"⚜️ Page 1/{len_result if len_result < max_pages else max_pages} ⚜️", callback_data="ignore")
         ])
         
         
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"<b>Found {(len_results)} Results For </b><code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
