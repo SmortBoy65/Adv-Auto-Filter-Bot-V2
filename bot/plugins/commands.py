aeec25a811f8538ece5891ad70f741c6fc4805a7
pdisk <83772019+pdisk@users.noreply.github.com>
2021-08-25 20:10:41 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -141,7 +141,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Join Our Channel ❤️', url='https://t.me/AAFchannel'
                                 )
                         ]
                     ]
